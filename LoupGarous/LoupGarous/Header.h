8949caf40f15e699e43dec8a46746a29a84b3589
Aymeric Goes <aymer.g@hotmail.fr>
2019-12-17 17:22:28 +0100

    added struct.h
    et les include aussi

--- LoupGarous/LoupGarous/Header.h
+++ LoupGarous/LoupGarous/Header.h
@@ -1,2 +1,11 @@
-#pragma once
-#include <stdio.h>
+#include<stdio.h>
+#include<winsock2.h>
+#include <windows.h>
+#include "Structures.h"
+#include <errno.h>
+#include <fcntl.h>
+
+
+
+#pragma warning (disable:4996)
+#pragma comment(lib,"ws2_32.lib")
