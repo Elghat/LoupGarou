d0dc2927585d041ee9036b63ac68bcf640efc0ea
Aymeric Goes <aymer.g@hotmail.fr>
2019-12-17 17:29:37 +0100

    structure joueur

--- LoupGarous/LoupGarous/Structures.h
+++ LoupGarous/LoupGarous/Structures.h
@@ -1,1 +1,10 @@
-#pragma once
+typedef struct monJoueur
+{
+	char pseudo[30];
+	bool alive;
+	char carte[255];
+	monJoueur* _next;
+	monJoueur* _previous;
+	bool isConnected;
+	char message[255];
+}joueur;
